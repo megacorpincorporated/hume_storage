9bf0b825d5b313b9786b6c19d55f897108298429
Måns Thörnvik <thornvik.mans@gmail.com>
2020-11-29 22:09:49 +0100

    Add start stop as package functions

--- hume_storage/__init__.py
+++ hume_storage/__init__.py
@@ -1,2 +1,2 @@
-from .data_store import register, save, get, get_all, delete
+from .data_store import register, save, get, get_all, delete, start, stop
 from .persistent.postgres import PersistentModel
